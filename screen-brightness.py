e84fd6b016427a80d8d3fc81866faceab8fac1d9
Oleksandr Boiko <oleksandr.boiko@i.ua>
2024-05-06 21:59:30 +0300

    transitive `screen-brightness`

--- screen-brightness.py
+++ screen-brightness.py
@@ -1,6 +1,16 @@
 from __future__ import annotations
 import subprocess
+import sys
+
+from command import TimeArgument
 import transition
+
+
+(channel,
+ from_brightness,
+ from_time,
+ to_brightness,
+ to_time) = sys.argv[1:6]
 
 
 class BrightnessState(transition.State):
@@ -11,10 +21,22 @@ class BrightnessState(transition.State):
         return self.__brightness == other.__brightness
 
     def apply(self) -> None:
-        subprocess.run(["brightness", str(self.__brightness)])
+        subprocess.run(["brightness", "set", channel, str(self.__brightness)])
 
     @staticmethod
     def avg(a: BrightnessState, b: BrightnessState, weight_a: float) -> BrightnessState:
         return BrightnessState(BrightnessState._value(a.__brightness,
                                                       b.__brightness,
                                                       weight_a))
+
+
+def main() -> None:
+    trans = transition.Transition(BrightnessState(int(from_brightness)),
+                                  TimeArgument().convert(from_time),
+                                  BrightnessState(int(to_brightness)),
+                                  TimeArgument().convert(to_time))
+    trans.run()
+
+
+if __name__ == "__main__":
+    main()
